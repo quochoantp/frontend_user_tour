141c6edef1b821cbacde0f9f244dd6cce88997dd
hoandq <hoandq@neo.vn>
2023-07-30 02:30:18 +0700

    fix

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@
 
     <div class="mt-16">
       <router-view></router-view>
+      <!-- Messenger Plugin chat Code -->
+      <div id="fb-root"></div>
+
+      <!-- Your Plugin chat code -->
+      <div id="fb-customer-chat" class="fb-customerchat"></div>
     </div>
 
     <Footer></Footer>
@@ -18,6 +23,28 @@ export default {
     AppBar: () => import("@/components/layout/appbar.vue"),
     Footer: () => import("@/components/layout/footer.vue"),
   },
-  data: () => ({}),
+  data: () => ({
+    chatboxId: "fb-customer-chat",
+    pageId: "109181652265047",
+    attribution: "biz_inbox",
+  }),
+  mounted() {
+    window.fbAsyncInit = function () {
+      FB.init({
+        xfbml: true,
+        version: "v17.0",
+      });
+    };
+
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  },
 };
 </script>
